d00470430952dc285d5466c964ce242ace1cb2f8
mansoorkhan108 <khan@progton.com>
2021-07-30 14:17:18 +0530

    mobile constraints

---
+++ db/migrate/20210730075543_add_mobile_constraints.rb
@@ -0,0 +1,9 @@
+class AddMobileConstraints < ActiveRecord::Migration[6.1]
+  def up
+    execute "ALTER TABLE users ADD CONSTRAINT check_mobile_length CHECK (mobile >= 6000000000 AND mobile <= 9999999999 )"
+  end
+
+  def down
+    execute "ALTER TABLE users DROP CONSTRAINT check_mobile_length"
+  end
+end
